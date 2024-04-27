3a752dee75dca695d04c249509d45c76be35b4da
Adriel Fariña <135282974+adriel9-5@users.noreply.github.com>
2024-04-27 13:41:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function playSonido (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+   const instrumento = tecla.classList[1];
+
+   const idAudio = `#sonido_${instrumento}`;
+   console.log(idAudio)
+
+   tecla.onclick = function (){
+    playSonido(idAudio);
+   }; 
+   contador = contador + 1;
+
+   tecla.onkeydown = function(evento){
+      
+       if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('activa');
+       }
+
+       console.log(evento.code === 'Space' || evento.code === 'Enter')
+      
+   };
+
+   tecla.onkeyup = function(){
+       tecla.classList.remove('activa');
+   }
+
+
+};
+
+
+
+
